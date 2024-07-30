0ac78854561a76e9cadd844bf409aae06d1ab45a
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-07-30 15:00:51 -0500

    Fixed passUserToView and isSignedIn

--- middleware/pass-user-to-view.js
+++ middleware/pass-user-to-view.js
@@ -3,6 +3,6 @@
 const passUserToView = (req, res, next) => {
     res.locals.user = req.session.user ? req.session.user : null
     next()
-}
+};
   
-module.exports = passUserToView
+module.exports = passUserToView;
